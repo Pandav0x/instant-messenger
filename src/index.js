564aa18201c559a919ff3ef83f1e529a9f2a65d4
Panda <pandav0x@hotmail.fr>
2021-05-22 12:30:07 +0200

    Add basic node server

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 'use strict';
 
-console.log('hello');
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.Server(app);
+
+app.get('/', function (req, res) {
+    res.send('Hello');
+});
+
+server.listen(80, function(){
+    console.log('Server started.');
+});
